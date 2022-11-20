fe08bc17511ba959ca2597678cfde1b44a6553c4
brunodias <brunocesarodias@hotmail.com>
2022-11-20 18:44:33 -0300

    Ajustes no layout

--- TaskBoardFront/src/app/task/task-list/task-list.component.ts
+++ TaskBoardFront/src/app/task/task-list/task-list.component.ts
@@ -1,49 +1,91 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskApiService } from '../task-service/task-api.service';
-import { Task, TaskStatus } from '../models/task-model'
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Task, TaskStatus } from '../models/task-model';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.css']
+  styleUrls: ['./task-list.component.css'],
 })
-
 export class TaskListComponent implements OnInit {
+  Instance: TaskListComponent = this;
   ApiService: TaskApiService;
-  private _tasks:Task[] = [];
+  private _tasks: Task[] = [];
   PendingTasks: Task[] = [];
   ActiveTasks: Task[] = [];
   ClosedTasks: Task[] = [];
 
-  constructor(apiService:TaskApiService) { 
+  constructor(apiService: TaskApiService) {
     this.ApiService = apiService;
+  }
+
+  private RefreshTasks() {
+    this.PendingTasks = this.GetTasksWithStatus(0);
+    this.ActiveTasks = this.GetTasksWithStatus(1);
+    this.ClosedTasks = this.GetTasksWithStatus(2);
   }
 
   async ngOnInit(): Promise<void> {
     this._tasks = await this.ApiService.GetTasks();
-    this.PendingTasks = this.GetTasksWithStatus(0);
-    this.ActiveTasks = this.GetTasksWithStatus(1);
-    this.ClosedTasks = this.GetTasksWithStatus(2);
-    console.log(this.PendingTasks);
-    console.log(this.ActiveTasks);
-    console.log(this.ClosedTasks);
+    this.RefreshTasks();
   }
 
-  private GetTasksWithStatus = (status:number):Task[]=>{
-    return this._tasks.filter(x=>x.Status == status);
-  }
+  private GetTasksWithStatus = (status: number): Task[] => {
+    return this._tasks.filter((x) => x.Status == status);
+  };
 
-  ChangeColumnEvent($event:CdkDragDrop<Task[]>){
+  async ChangeColumnEvent($event: CdkDragDrop<Task[]>) {
+    var taskId = $event.container.data[$event.previousIndex];
     if ($event.previousContainer === $event.container) {
-      moveItemInArray($event.container.data, $event.previousIndex, $event.currentIndex);
+      moveItemInArray(
+        $event.container.data,
+        $event.previousIndex,
+        $event.currentIndex
+      );
     } else {
       transferArrayItem(
         $event.previousContainer.data,
         $event.container.data,
         $event.previousIndex,
-        $event.currentIndex,
+        $event.currentIndex
       );
     }
+  }
+
+  ChangeStatusTaskFromCombo($event: any) {
+    var task = this._tasks.find((x) => x.Id == $event.TaskId);
+    if (task == null) return;
+
+    var taskFromArray =
+      task.Status == 0
+        ? this.PendingTasks
+        : task.Status == 1
+        ? this.ActiveTasks
+        : this.ClosedTasks;
+
+    var taskPreviousIndex = taskFromArray.findIndex(
+      (x) => x.Id == $event.TaskId
+    );
+
+    task.Status = $event.NewStatus;
+
+    var taskToArray =
+      task.Status == 0
+        ? this.PendingTasks
+        : task.Status == 1
+        ? this.ActiveTasks
+        : this.ClosedTasks;
+
+    transferArrayItem(taskFromArray, taskToArray, taskPreviousIndex, 0);
+  }
+
+  ChangePriorityTaskFromCombo($event: any) {
+    var task = this._tasks.find((x) => x.Id == $event.TaskId);
+    if (task != null) task.Priority = $event.NewPriority;
   }
 }
--- TaskBoardFront/src/app/task/task-resume/task-resume.component.ts
+++ TaskBoardFront/src/app/task/task-resume/task-resume.component.ts
@@ -1,15 +1,39 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { Task } from '../models/task-model';
+import { TaskListComponent } from '../task-list/task-list.component';
 
 @Component({
   selector: 'app-task-resume',
   templateUrl: './task-resume.component.html',
-  styleUrls: ['./task-resume.component.css']
+  styleUrls: ['./task-resume.component.css'],
 })
 export class TaskResumeComponent {
-  @Input() Task:Task = new Task();
+  @Input() Task: Task = new Task();
+  @Input() BorderColor: string = '';
 
-  ChangePriority($event:any){
+  @Output()
+  OnTaskChangePriority = new EventEmitter();
+  ChangePriority($event: any) {
     this.Task.Priority = $event.target.value;
+    this.OnTaskChangePriority.emit({
+      TaskId: this.Task.Id,
+      NewPriority: this.Task.Priority,
+    });
+  }
+
+  @Output()
+  OnTaskChangeStatus = new EventEmitter();
+  ChangeStatus($event: any) {
+    this.OnTaskChangeStatus.emit({
+      TaskId: this.Task.Id,
+      NewStatus: $event.target.value,
+    });
+    this.Task.Status = $event.target.value;
   }
 }
